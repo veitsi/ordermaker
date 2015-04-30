b88aba52b13b7bfbcf3f4bea5434fcaf2a914994
xtfkpi <xtfkpi@xtfkpi-Lenovo-E10-30>
2015-04-30 13:21:22 +0300

    01

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -3,7 +3,9 @@ package hello;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,7 +27,8 @@ public class WebController extends WebMvcConfigurerAdapter {
     }
 
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public String checkPersonInfo(@Valid Person person, BindingResult bindingResult, Order order) {
+    public String checkPersonInfo(@Valid Person person, BindingResult bindingResult,
+    		@ModelAttribute Order order, Model model ) {
         if (bindingResult.hasErrors()) {
             return "form";
         }
